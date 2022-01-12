a3f13b6d22e0e6464d659bede23c277bde556342
ccastillo-id <carl.casitllo@id-edd.com>
2022-01-12 00:01:52 -0700

    Test Register API

---
+++ Test-RegisterAPI.ps1
@@ -0,0 +1,33 @@
+﻿function Test-RegisterAPI 
+{
+    param(
+        $InputObject
+    )
+
+    # Need to capture error when given not same password
+    # Error is being caught, need to store in variable and return.
+
+    $uri = 'http://localhost:8000/api/register'
+
+    $returnObject = @()
+
+    try 
+    {
+        $data = Invoke-WebRequest -Uri $uri -Method Post -ContentType 'application/json' -Body $InputObject
+        return $data
+    }
+    catch
+    {
+        return $data
+    }
+    
+}
+
+$jsonObject = [ordered]@{
+    first_name = 'Carl'
+    last_name = 'Castillo'
+    email = 'carlcastillo@me.com'
+    password = '123'
+    password_confirm = '1234'
+} | ConvertTo-Json
+Test-RegisterAPI -InputObject $jsonObject
